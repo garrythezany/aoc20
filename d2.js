6efc704056bda363ff786ee02e8fae958183adca
Garret Hohmann <ghohmann@uos.de>
2020-12-03 21:25:03 +0100

    Update d2.js
    normal cuts

--- d2.js
+++ d2.js
@@ -14,5 +14,5 @@ var input = `7-8 c: vwcvkcct
 4-11 p: rzlgxshhpbp
 1-4 b: qbbbbbbbbbbb`;
 
-console.log({"p1":input.split("\n").map(x=>{hx = x.split(": "); return hx}).map(y=>{h=y[0].split(" ");hl=h[0].split("-");return{min:Number(hl[0]),max:Number(hl[1]),let:h[1],pw:y[1]}}).filter((z)=>{hm = z.pw.match(new RegExp(z.let, "g"));return hm?hm.length<z.min?false:hm.length>z.max?false:true:false;}).length});
-console.log({"p2":input.split("\n").map(x=>{hx = x.split(": "); return hx}).map(y=>{h=y[0].split(" ");hl=h[0].split("-");return{min:Number(hl[0])-1,max:Number(hl[1])-1,let:h[1],pw:y[1]}}).filter((z)=>{return (z.pw[z.min] === z.let && z.pw[z.max] === z.let)||(z.pw[z.min] !== z.let && z.pw[z.max] !== z.let)?false:(z.pw[z.min] !== z.let && z.pw[z.max] === z.let) || (z.pw[z.min] === z.let && z.pw[z.max] !== z.let)?true:false;}).length});
+console.log({"p1":input.split("\n").map(x=>x.split(": ")).map(y=>{h=y[0].split(" ");hl=h[0].split("-");return{min:Number(hl[0]),max:Number(hl[1]),let:h[1],pw:y[1]}}).filter((z)=>{hm = z.pw.match(new RegExp(z.let, "g"));return hm?hm.length<z.min?false:hm.length>z.max?false:true:false;}).length});
+console.log({"p2":input.split("\n").map(x=>x.split(": ")).map(y=>{h=y[0].split(" ");hl=h[0].split("-");return{min:Number(hl[0])-1,max:Number(hl[1])-1,let:h[1],pw:y[1]}}).filter((z)=>{return (z.pw[z.min] === z.let && z.pw[z.max] === z.let)||(z.pw[z.min] !== z.let && z.pw[z.max] !== z.let)?false:(z.pw[z.min] !== z.let && z.pw[z.max] === z.let) || (z.pw[z.min] === z.let && z.pw[z.max] !== z.let)?true:false;}).length});
